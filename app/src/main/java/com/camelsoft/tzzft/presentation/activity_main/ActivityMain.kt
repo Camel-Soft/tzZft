fb095b9975648ceef6f00a55b6f86d0a770c85fc
Maximus <accauntmmg@gmail.com>
2023-01-14 15:29:40 +0400

    second commit

--- app/src/main/java/com/camelsoft/tzzft/presentation/activity_main/ActivityMain.kt
+++ app/src/main/java/com/camelsoft/tzzft/presentation/activity_main/ActivityMain.kt
@@ -1,12 +1,57 @@
 package com.camelsoft.tzzft.presentation.activity_main
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.camelsoft.tzzft.R
+import com.camelsoft.tzzft.databinding.ActivityMainBinding
+import com.camelsoft.tzzft.presentation.dialogs.showError
+import com.camelsoft.tzzft.presentation.dialogs.showInfo
+import dagger.hilt.android.AndroidEntryPoint
+import java.lang.ref.WeakReference
 
+@AndroidEntryPoint
 class ActivityMain : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var weakContext: WeakReference<Context>
+    private val viewModel: ActivityMainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        weakContext = WeakReference<Context>(this)
+        supportActionBar?.title = resources.getString(R.string.title)
+
+        handleEventsUi()
+        btnClick()
+    }
+
+    private fun handleEventsUi() {
+        lifecycleScope.launchWhenStarted {
+            viewModel.eventsUi.collect { event ->
+                when (event) {
+                    is EventUiMainActivity.ShowError -> showError(weakContext.get()!!, event.message) {}
+                    is EventUiMainActivity.ShowInfo -> showInfo(weakContext.get()!!, event.message) {}
+                    is EventUiMainActivity.ShowProgress -> {}
+                    is EventUiMainActivity.PublicBankInfo -> showInfo(weakContext.get()!!, event.bankInfo.brand!!) {}
+                }
+            }
+        }
+    }
+
+    private fun btnClick() {
+        binding.btnStart.setOnClickListener {
+            if (binding.editBin.text.toString().length < 6) {
+                showInfo(weakContext.get()!!, resources.getString(R.string.helper_text)) {}
+                return@setOnClickListener
+            }
+            viewModel.eventsVm(EventVmMainActivity.PerformRequest(
+                bin = binding.editBin.text.toString().toInt()
+            ))
+        }
     }
 }
--- app/src/main/java/com/camelsoft/tzzft/presentation/activity_main/ActivityMainViewModel.kt
+++ app/src/main/java/com/camelsoft/tzzft/presentation/activity_main/ActivityMainViewModel.kt
@@ -2,8 +2,10 @@ package com.camelsoft.tzzft.presentation.activity_main
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.camelsoft.tzzft.domain.use_cases.use_case_info.EventProgress
 import com.camelsoft.tzzft.presentation.api.IInfoApi
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
@@ -17,13 +19,29 @@ class ActivityMainViewModel @Inject constructor(
     private val _eventsUi = Channel<EventUiMainActivity>()
     val eventsUi = _eventsUi.receiveAsFlow()
 
+//    private val _mInfo = MutableLiveData<MInfo>()
+//    val mInfo: LiveData<MInfo> = _mInfo
 
-
-
-
-
-
-
+    fun eventsVm(event: EventVmMainActivity) {
+        when (event) {
+            is EventVmMainActivity.PerformRequest -> {
+                viewModelScope.launch(Dispatchers.IO) {
+                    iInfoApi.getInfo(bin = event.bin).collect {
+                        when (it) {
+                            is EventProgress.Success ->
+                                sendEventUi(EventUiMainActivity.PublicBankInfo(it.data))
+                            is EventProgress.UnSuccess ->
+                                sendEventUi(EventUiMainActivity.ShowInfo(it.message))
+                            is EventProgress.Progress ->
+                                sendEventUi(EventUiMainActivity.ShowProgress(it.show))
+                            is EventProgress.Error ->
+                                sendEventUi(EventUiMainActivity.ShowError(it.message))
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     private fun sendEventUi(event: EventUiMainActivity) {
         viewModelScope.launch {
